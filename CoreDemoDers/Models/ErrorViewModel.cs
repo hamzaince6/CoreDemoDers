355ed2df91676b2beb7d8d5dec28080be119e254
hamza <hamza@DESKTOP-HFDE1OF>
2023-05-10 11:12:19 +0300

    Add project files.

---
+++ CoreDemoDers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDemoDers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
